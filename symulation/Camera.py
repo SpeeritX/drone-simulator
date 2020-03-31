cd5df77b99a25c680a07fa9d0c5113c77d7b965e
Speerit <jedrek.185@gmail.com>
2020-03-31 23:00:31 +0200

    Added requirements.txt, first steps in creating responsive camera.

--- symulation/Camera.py
+++ symulation/Camera.py
@@ -4,13 +4,20 @@
 # Created by Milosz Glowaczewski on 28.03.2020.
 # All rights reserved.
 #
+from math import sqrt
 
+import numpy
 import pygame
 import typing
 from pygame.rect import Rect
 from pygame.surface import Surface
 
+
 class Camera:
+    # Distance from center of the screen required for camera to start following the player.
+    MIN_DIST_TO_MOVE_CAM = 100
+    CAMERA_SPEED = 10
+
     def __init__(self, width: typing.List, height):
 
         self.width = width
@@ -22,4 +29,20 @@ class Camera:
 
     def draw(self, image: Surface, rect: Rect):
         pass
+
+    def update(self, playerPosX, playerPosY):
+        distX = self.offsetX - playerPosX
+        distY = self.offsetY - playerPosY
+
+        distance = sqrt(distX ** 2 + distY ** 2)
+
+        if distance <= self.MIN_DIST_TO_MOVE_CAM:
+            pass
+
+
+
+
+    def getOffset(self):
+        return self.offsetX, self.offsetY
+
 
