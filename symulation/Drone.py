7a64eccf5d47eb23d4fcb09c29f6bf6da1de418a
Jędrek <jedrek.185@gmail.com>
2020-05-21 17:01:15 +0200

    Rotors color based on current engine power.

--- symulation/Drone.py
+++ symulation/Drone.py
@@ -23,10 +23,12 @@ import pygame
 class Drone(Entity):
 
     CHASSISWIDTH = 144
-    CHASSISHEIGHT = 10
+    CHASSISHEIGHT = 41
     ENGINESIZE = 12
     FRICTION = 0.5
-    DRONE_SPRITE_PATH = 'resources\\Sprites\\Drone.png'
+    DRONE_BASE_SPRITE_PATH = 'resources\\Sprites\\DroneBase.png'
+    DRONE_L_ROTOR_SPRITE_PATH = 'resources\\Sprites\\LeftRotor.png'
+    DRONE_R_ROTOR_SPRITE_PATH = 'resources\\Sprites\\RightRotor.png'
 
     def __init__(self, mass, moment, spaceGravity, position, aiComponent: AIComponent):
 
@@ -41,7 +43,9 @@ class Drone(Entity):
         self.chassis.friction = self.FRICTION
         self.currentDecision = AIDecision(0, 0)
 
-        self.image = pygame.image.load(self.DRONE_SPRITE_PATH)
+        self.droneBase = pygame.image.load(self.DRONE_BASE_SPRITE_PATH)
+        self.leftRotor = pygame.image.load(self.DRONE_L_ROTOR_SPRITE_PATH)
+        self.rightRotor = pygame.image.load(self.DRONE_R_ROTOR_SPRITE_PATH)
         self.blendColors((255, 0, 0), (0, 255, 0), 0.5)
         # self.sprite.set_colorkey((0, 0, 0))
 
@@ -62,24 +66,49 @@ class Drone(Entity):
             self.currentDecision = self.aiController.getDecision()
             self.aiController.update(DroneState(self.body, targetAngle))
 
+    def colorizeSurface(self, surface, color):
+        rect = surface.get_rect()
+        colorImage = pygame.Surface(rect.size).convert_alpha()
+        colorImage.fill(color)
+        colorImage.blit(surface, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+        return colorImage
+
     def blendColors(self, a, b, t):
         """ Linearly interpolates between colors a and b by t.
-            t is clamped between 0 and 1. When t is 0 returns a. When t is 1 returns b. """
+            t is clamped between 0 and 1. When t is 0 color is a. When t is 1 color is b.
+            Applies calculated color on surface """
         result = tuple(a[i] + (b[i] - a[i]) * t for i in range(3))
         return result
 
+    def extendSurface(self, surface, size=4):
+        rect = surface.get_rect()
+        newSurface = pygame.Surface((rect.size[0] + size, rect.size[1] + size), pygame.SRCALPHA)
+        newSurface.blit(surface, (size/2, size/2))
+        return newSurface
+
     def getDroneSprite(self):
         # set scale
-        scaledImage = scaleImage(self.image, (self.CHASSISWIDTH, self.CHASSISHEIGHT))
+        #scaledImage = scaleImage(self.droneBase, (self.CHASSISWIDTH, self.CHASSISHEIGHT))
         # set rotation
-        finalImage = pygame.transform.rotozoom(scaledImage, math.degrees(self.body.angle), 1)
-        rect = finalImage.get_rect()
-        colorImage = pygame.Surface(rect.size).convert_alpha()
 
-        colorFactor = min(max(self.leftEngine.getForce(), self.rightEngine.getForce())*20, 1)
-        newColor = self.blendColors((0, 255, 0), (240, 30, 0), colorFactor)
-        colorImage.fill(newColor)
-        finalImage.blit(colorImage, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+        # create surface
+        image = pygame.Surface((self.CHASSISWIDTH, self.CHASSISHEIGHT), pygame.SRCALPHA)
+        # add base
+        image.blit(self.droneBase, ((self.CHASSISWIDTH - self.droneBase.get_rect().size[0])/2,
+                                    self.CHASSISHEIGHT - self.droneBase.get_rect().size[1]))
+        # add left rotor
+        colorFactor = min(self.leftEngine.getForce() * 20, 1)
+        rotorColor = self.blendColors((0, 255, 0), (240, 30, 0), colorFactor)
+        leftRotor = self.colorizeSurface(self.leftRotor, rotorColor)
+        image.blit(leftRotor, (0, 0))
+
+        # add right rotor
+        colorFactor = min(self.rightEngine.getForce() * 20, 1)
+        rotorColor = self.blendColors((0, 255, 0), (240, 30, 0), colorFactor)
+        rightRotor = self.colorizeSurface(self.rightRotor, rotorColor)
+        image.blit(rightRotor, ((self.CHASSISWIDTH - rightRotor.get_rect().size[0]), 0))
+        finalImage = self.extendSurface(image)
+        finalImage = pygame.transform.rotozoom(finalImage, math.degrees(self.body.angle), 1)
         return finalImage.convert_alpha()
 
     def getLeftEnginePosition(self):
